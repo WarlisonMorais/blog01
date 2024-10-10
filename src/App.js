e3e26dd99ced6781e6498559f22884df39626e82
Warlison Morais <warlisonmorais0426@gmail.com>
2024-10-10 07:45:29 -0300

    Initial commit and create the structure.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="menu">
+      <h1>Hello World</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni suscipit, dolorum soluta magnam unde corrupti praesentium tempore quas quos nulla totam nobis deserunt necessitatibus! Delectus earum vero ad perferendis incidunt.</p>
     </div>
+    
   );
 }
 
